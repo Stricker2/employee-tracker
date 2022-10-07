8059df86b059579547198780f5f7b7bfe44aeffc
Stricker2 <101269390+Stricker2@users.noreply.github.com>
2022-10-07 17:22:47 -0400

    add department names, role titles, and first and last names to seeds

---
+++ db/seeds.sql
@@ -0,0 +1,29 @@
+INSERT INTO department (name)
+VALUES ("Engineering"),
+       ("Finance"),
+       ("Legal"),
+       ("Sales");
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Account Manager"),
+       ("Accountant"),
+       ("Lawyer"),
+       ("Legal Team Lead"),
+       ("Lead Engineer"),
+       ("Software Engineer"),
+       ("Sales Lead"),
+       ("Salesperson");
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("David", "De Gea"),
+       ("Diogo", "Dalot"),
+       ("Raphael", "Varane"),
+       ("Lisandro", "Martinez"),
+       ("Tyrell", "Malacia"),
+       ("Christian", "Eriksen"),
+       ("Scott", "McTominay"),
+       ("Bruno", "Fernandes"),
+       ("Marcus", "Rashford"),
+       ("Jadon", "Sancho"),
+       ("Anthony", "Martial");
+
