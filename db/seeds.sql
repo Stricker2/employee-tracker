97c6144c0b27b941aaf018c355a6de16502b6492
Stricker2 <101269390+Stricker2@users.noreply.github.com>
2022-10-07 21:50:16 -0400

    add dpmt ids, role ids, and manager ids

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,25 +5,25 @@ VALUES ("Engineering"),
        ("Sales");
 
 INSERT INTO roles (title, salary, department_id)
-VALUES ("Account Manager"),
-       ("Accountant"),
-       ("Lawyer"),
-       ("Legal Team Lead"),
-       ("Lead Engineer"),
-       ("Software Engineer"),
-       ("Sales Lead"),
-       ("Salesperson");
+VALUES ("Account Manager", 80000.00, 2),
+       ("Accountant", 65000.00, 2),
+       ("Lawyer", 150000.00, 3),
+       ("Legal Team Lead", 175000.00, 3),
+       ("Lead Engineer"120000.00, 1),
+       ("Software Engineer"90000.00, 1),
+       ("Sales Lead"75000.00, 4),
+       ("Salesperson"60000.00, 4);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("David", "De Gea"),
-       ("Diogo", "Dalot"),
-       ("Raphael", "Varane"),
-       ("Lisandro", "Martinez"),
-       ("Tyrell", "Malacia"),
-       ("Christian", "Eriksen"),
-       ("Scott", "McTominay"),
-       ("Bruno", "Fernandes"),
-       ("Marcus", "Rashford"),
-       ("Jadon", "Sancho"),
-       ("Anthony", "Martial");
+VALUES ("David", "De Gea", 4,),
+       ("Diogo", "Dalot", 3, 4),
+       ("Raphael", "Varane", 3, 4),
+       ("Lisandro", "Martinez", 1,),
+       ("Tyrell", "Malacia", 2, 1),
+       ("Christian", "Eriksen", 5,),
+       ("Scott", "McTominay", 8, 7),
+       ("Bruno", "Fernandes", 8, 7),
+       ("Marcus", "Rashford", 7,),
+       ("Jadon", "Sancho", 6, 5),
+       ("Anthony", "Martial", 6, 5);
 
